d6934e77db9c7143f13d6b5a0b3c5d2cf1605434
chenyan <chenyan@kaihong.com>
2023-05-17 15:42:49 +0800

    1

--- src/common/websocket_enum.py
+++ src/common/websocket_enum.py
@@ -5,6 +5,6 @@ class WebsocketEnum(Enum):
     '''
     Websocket使用的常量
     '''
-    WEBSOCKET_HOST = "71.255.2.21"
+    WEBSOCKET_HOST = "0.0.0.0"
     WEBSOCKET_PORT = 5678
     WEBSOCKET_TIMEOUT_ENUM = 10
